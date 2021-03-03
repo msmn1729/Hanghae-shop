b744578f0e4498e412a776f4bdde4c04dc4ca788
sanyoni <ksyline1997@gmail.com>
2021-03-03 15:36:52 +0900

    feat: 메인 페이지에서 요청할 수 있는 서버 응답 기능 구현

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('main.html')
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/goods/search', methods=['GET'])
+def getGoodsSearch():
+    received_keywords = request.args.get("keywords");
+    print('searched keywords: ', received_keywords);
+    return render_template('goods.html', keyword=received_keywords);
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
