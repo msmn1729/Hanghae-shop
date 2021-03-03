8eb4aa4e1292e713dc1c970aed0fae0a3ecea2d2
sanyoni <ksyline1997@gmail.com>
2021-03-03 22:07:14 +0900

    feat: 클라이언트가 요청한 login정보가 올바를 때 토큰을 만들어 돌려주는 기능 구현

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import hashlib
-
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import hashlib
+import jwt
+import datetime
+
+SECRET_KEY = 'Pl^EqCCvnI(d3xDBFofHyxHxLtuBWs';
 
 app = Flask(__name__)
-
-from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.hanghaeshop
@@ -27,8 +29,25 @@ def userLogin():
 
     hashed_password = hashlib.sha256(received_password.encode('utf-8')).hexdigest()
 
-    print(received_id, received_password, hashed_password)
-    return jsonify({'success': True, 'message': '로그인에 성공하였습니다.'})
+    finded_user = db.user.find_one({'id': received_id, 'password': hashed_password})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if finded_user is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': received_id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'success': True, 'message': '로그인에 성공하였습니다.', 'token': token})
+
+    return jsonify({'success': False, 'message': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 @app.route('/user/register', methods=['GET'])
