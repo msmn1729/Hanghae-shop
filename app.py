d1a240f0e51b12b47d67a25d9ae1414ea5c64ef7
sanyoni <ksyline1997@gmail.com>
2021-03-04 23:12:42 +0900

    refactor: 포매팅 변경

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ db = client.hanghaeshop
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def tryGetUserInfoWithToken(received_token: str):
